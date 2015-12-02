1f5098be84326f5ccd157ad1e4a809c9ba309b28
Kai LIU <kiwenlau@126.com>
2015-12-02 12:53:59 +0900

    Update run-pipeline.sh

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -17,15 +17,15 @@ sudo docker run -v $current_path/data:/tmp/data -v $current_path/output:/tmp/out
 echo -e "\n\n"
 sudo docker run -v $current_path/data:/tmp/data -v $current_path/output:/tmp/output -w /tmp kiwenlau/tophat-cufflinks tophat2 -o output/tophat_out_liver output/index/genome_index data/ggal_liver_1.fq data/ggal_liver_2.fq
 
-echo "\n\n\n\nStep 3. Assembles the transcript by using the cufflinks\n"
+echo -e "\n\n\n\nStep 3. Assembles the transcript by using the cufflinks\n"
 sudo docker run -v $current_path/data:/tmp/data -v $current_path/output:/tmp/output -w /tmp kiwenlau/tophat-cufflinks cufflinks -o output/cufflinks_out_gut output/tophat_out_gut/accepted_hits.bam
 echo -e "\n\n"
 sudo docker run -v $current_path/data:/tmp/data -v $current_path/output:/tmp/output -w /tmp kiwenlau/tophat-cufflinks cufflinks -o output/cufflinks_out_liver output/tophat_out_liver/accepted_hits.bam
 
-echo "\n\n\n\nStep 4\n"
+echo -e "\n\n\n\nStep 4. Run cuffmerge command\n"
 echo "output/cufflinks_out_gut/transcripts.gtf" > output/assemblies.txt
 echo "output/cufflinks_out_liver/transcripts.gtf" >> output/assemblies.txt
 sudo docker run -v $current_path/data:/tmp/data -v $current_path/output:/tmp/output -w /tmp kiwenlau/tophat-cufflinks cuffmerge -o output/merged_asm output/assemblies.txt
 
-#echo "Step 5"
+echo -e "\n\n\nStep 5. Run cuffdiff command\n"
 sudo docker run -v $current_path/data:/tmp/data -v $current_path/output:/tmp/output -w /tmp kiwenlau/tophat-cufflinks cuffdiff -o output/cuffdiff_out output/merged_asm/merged.gtf output/tophat_out_gut/accepted_hits.bam output/tophat_out_liver/accepted_hits.bam
